5a282f3738fab2703d2611de1e2eb2057625a254
Sherry Singh <118465547+hsecurities@users.noreply.github.com>
2026-06-01 17:20:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,39 @@
+#!/bin/bash
 
+echo "[+] Installing SmartCard Guard..."
+
+sudo apt update
+
+sudo apt install -y \
+pcscd \
+pcsc-tools \
+libnotify-bin
+
+sudo systemctl enable pcscd
+sudo systemctl start pcscd
+
+mkdir -p ~/.local/bin
+cp smartcard-guard.sh ~/.local/bin/
+chmod +x ~/.local/bin/smartcard-guard.sh
+
+mkdir -p ~/.config/systemd/user
+
+cat > ~/.config/systemd/user/smartcard-guard.service << EOF
+[Unit]
+Description=SmartCard Guard
+
+[Service]
+ExecStart=/home/$USER/.local/bin/smartcard-guard.sh
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl --user daemon-reload
+systemctl --user enable smartcard-guard.service
+systemctl --user start smartcard-guard.service
+
+echo ""
+echo "[+] SmartCard Guard Installed Successfully"
+echo "[+] Service Started"
